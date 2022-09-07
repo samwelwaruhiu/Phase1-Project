4d9cf67b96522b7afe35c698dc4e692bf34f8bc1
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-09-07 09:19:09 +0300

    identifying the public api ill use

---
+++ index.js
@@ -0,0 +1,9 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '492384d307msh27a478b294303fcp1e10d9jsn213fecda08a9',
+		'X-RapidAPI-Host': 'random-facts2.p.rapidapi.com'
+	}
+};
+
+fetch('https://random-facts2.p.rapidapi.com/getfact', options)
